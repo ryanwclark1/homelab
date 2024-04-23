a6af6edda3f34f761ce6d9629ec7154a0f79e113
Ryan Clark <ryanc@accentservices.com>
2024-04-23 12:54:54 -0500

    Add template_node variable and use SSH to start VMs

--- cloud-init/start.sh
+++ cloud-init/start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+template_node="james"
+
 # List of VM IDs to start
 vm_ids=(221 222 223 224 225 226 227 228 229 230 231 232 233 234 235 236)
 
@@ -7,7 +9,10 @@ vm_ids=(221 222 223 224 225 226 227 228 229 230 231 232 233 234 235 236)
 start_vm() {
     local vm_id=$1
     echo "Starting VM ID $vm_id..."
-    qm start $vm_id
+    ssh root@"$template_node.techcasa.io" "
+      qm start $vm_id
+      exit
+    "
 }
 
 # Loop over all VM IDs and start each one
