8fd9722b5dfd2ed99d0ffac59a9fd1c4b8e958b9
Ryan Clark <ryanc@accentservices.com>
2024-04-24 14:32:45 -0500

    Refactor SSH key configuration in deploy.sh

--- cloud-init/deploy.sh
+++ cloud-init/deploy.sh
@@ -95,10 +95,9 @@ for node in "${nodes[@]}"; do
         log_action "Configuring VM on $node_ip..."
         ssh "$USER@$node_ip" "
             temp_file=$(mktemp -t tmp_key.XXX) \
-            echo $SSH_KEY_TEXT > $temp_file \
-            cat ~/.ssh/id_rsa.pub >> $temp_file \
-            qm set $vm_id --sshkey $temp_file;
-            echo "stop";
+            echo $SSH_KEY_TEXT > $temp_file;
+            cat ~/.ssh/id_rsa.pub >> $temp_file;
+            qm set $vm_id --sshkey "$temp_file";
             qm set $vm_id --ipconfig0 ip=$vm_ip/$CIDR,gw=$GATEWAY;
             qm set $vm_id --tags "$TAG,$role";
             qm move-disk $vm_id scsi0 $disk;
