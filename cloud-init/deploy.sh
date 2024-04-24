88e6e21d34c0df391018721dddf1bb1a4a665024
Ryan Clark <ryanc@accentservices.com>
2024-04-24 12:28:12 -0500

    Fix SSH key file creation and disk resize in deploy script

--- cloud-init/deploy.sh
+++ cloud-init/deploy.sh
@@ -91,12 +91,12 @@ for node in "${nodes[@]}"; do
         log_action "Configuring VM on $node_ip..."
         ssh "$USER@$node_ip" "
             temp_file=$(mktemp);
-            echo $SSH_KEY_TEXT > "$temp_file"
+            echo "$SSH_KEY_TEXT" > "$temp_file";
             qm set $vm_id --ipconfig0 ip=$vm_ip/$CIDR,gw=$GATEWAY;
             qm set $vm_id --sshkey "$temp_file";
             qm set $vm_id --tags $TAG;
             qm move-disk $vm_id scsi0 $disk;
-            qm disk resize $vm_id scsi0 $disk_size
+            qm disk resize $vm_id scsi0 $disk_size;
             exit
         "
         log_action "VM $vm_name ($vm_id) deployed and configured at $vm_ip."
