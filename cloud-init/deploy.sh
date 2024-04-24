1573872132bb6089aa8a11ac9eb84263f3ac6ef6
Ryan Clark <ryanc@accentservices.com>
2024-04-24 15:17:43 -0500

    Refactor SSH key handling in deploy script

--- cloud-init/deploy.sh
+++ cloud-init/deploy.sh
@@ -98,12 +98,11 @@ for node in "${nodes[@]}"; do
             qm set $vm_id --tags "$TAG,$role";
             qm move-disk $vm_id scsi0 $disk;
             qm disk resize $vm_id scsi0 $disk_size;
-            'TEMP_FILE="/tmp/key_temp.txt"';
-            'touch "${TEMP_FILE}"';
-            'echo $SSH_KEY_TEXT > "${TEMP_FILE}"';
-            'cat ~/.ssh/id_rsa.pub >> "${TEMP_FILE}"';
-            qm set $vm_id --sshkey "${TEMP_FILE}";
-            rm "${TEMP_FILE}"
+            temp_file=$(mktemp -t tmp_key.XXX);
+            echo $SSH_KEY_TEXT > $temp_file;
+            cat ~/.ssh/id_rsa.pub >> $temp_file;
+            qm set 223 --sshkey "$temp_file";
+            rm "${temp_file}"
         "
         log_action "VM $vm_name ($vm_id) deployed and configured at $vm_ip."
     done
