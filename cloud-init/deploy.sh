86c5dae1ff1b073e000dd0e0003d3929f0c7d3b5
Ryan Clark <ryanc@accentservices.com>
2024-04-24 12:24:22 -0500

    Update deploy.sh script to fix SSH key file path

--- cloud-init/deploy.sh
+++ cloud-init/deploy.sh
@@ -93,9 +93,9 @@ for node in "${nodes[@]}"; do
             echo "LOGIN";
             temp_file=$(mktemp);
             echo temp_file;
-            echo $SSH_KEY_TEXT > $temp_file;
+            echo $SSH_KEY_TEXT > "/$temp_file";
             qm set $vm_id --ipconfig0 ip=$vm_ip/$CIDR,gw=$GATEWAY;
-            qm set $vm_id --sshkey $temp_file;
+            qm set $vm_id --sshkey "/$temp_file";
             qm set $vm_id --tags $TAG;
             qm move-disk $vm_id scsi0 $disk;
             qm disk resize $vm_id scsi0 $disk_size;
