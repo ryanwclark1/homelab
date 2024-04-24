6def1f1c0aa6df4b0080a32787518a1b471c55c1
Ryan Clark <ryanc@accentservices.com>
2024-04-24 12:50:53 -0500

    Fix SSH key configuration and IP address assignment***

--- cloud-init/deploy.sh
+++ cloud-init/deploy.sh
@@ -92,8 +92,8 @@ for node in "${nodes[@]}"; do
         ssh "$USER@$node_ip" "
             temp_file=$(mktemp -t tmp_key.XXX) \
             echo $SSH_KEY_TEXT > $temp_file \
-            qm set $vm_id --ipconfig0 ip=$vm_ip/$CIDR,gw=$GATEWAY;
             qm set $vm_id --sshkey $temp_file;
+            qm set $vm_id --ipconfig0 ip=$vm_ip/$CIDR,gw=$GATEWAY;
             qm set $vm_id --tags $TAG;
             qm move-disk $vm_id scsi0 $disk;
             qm disk resize $vm_id scsi0 $disk_size;
