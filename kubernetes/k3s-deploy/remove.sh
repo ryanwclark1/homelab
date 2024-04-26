63bf44f663e62ec46fcad03982ed0575440ceea0
Ryan Clark <ryanc@accentservices.com>
2024-04-26 09:29:28 -0500

    Refactor kubeconfig path and safely remove files

--- kubernetes/k3s-deploy/remove.sh
+++ kubernetes/k3s-deploy/remove.sh
@@ -4,12 +4,19 @@
 CURRENT_USER=$(whoami)
 
 # Set the kubeconfig path
-KUBECONFIG_PATH="/home/$CURRENT_USER/.kube/config"
+KUBECONFIG_PATH="/home/$CURRENT_USER/.kube"
 
 # Remove the export statement from the appropriate shell configuration file
 remove_export_statement() {
-    echo "Removing export statement from $1"
-    sed -i "/export KUBECONFIG=$KUBECONFIG_PATH/d" "$1"
+    local file="$1"
+    local pattern="export KUBECONFIG=${KUBECONFIG_PATH}/config"
+    echo "Checking for the export statement in ${file}"
+    if grep -q "$pattern" "$file"; then
+        echo "Removing export statement from ${file}"
+        sed -i "/$pattern/d" "$file"
+    else
+        echo "No export statement found in ${file}. Skipping..."
+    fi
 }
 
 # Determine the shell and the corresponding configuration file
@@ -34,10 +41,27 @@ esac
 # Remove the export statement from the configuration file
 remove_export_statement "$CONFIG_FILE"
 
+
+# Function to safely remove files or directories
+safe_rm() {
+    local target="$1"
+    if [ -e "$target" ]; then
+        rm -rf "$target"
+        echo "Removed ${target}"
+    else
+        echo "Target ${target} not found. Skipping..."
+    fi
+}
+
 # Remove the kubeconfig file and directory
-rm -rf "$KUBECONFIG_PATH"
-rm "$HOME/kube-vip.yaml"
-rm "$HOME/ipAddressPool.yaml"
+safe_rm "$XDG_CACHE_HOME/helm"
+safe_rm "$XDG_CONFIG_HOME/helm"
+safe_rm "$XDG_DATA_HOME/helm"
+safe_rm "$KUBECONFIG_PATH"
+safe_rm "/usr/local/bin/kubectl"
+safe_rm "/usr/local/bin/k3sup"
+safe_rm "$HOME/kube-vip.yaml"
+safe_rm "$HOME/ipAddressPool.yaml"
 
 # If it's a master node, kill k3s
 if [ -f "/etc/rancher/k3s.yaml" ]; then
