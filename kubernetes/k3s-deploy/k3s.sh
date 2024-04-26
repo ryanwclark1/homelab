7596e39a327ca793864a7472ded986a5375f6a54
Ryan Clark <ryanc@accentservices.com>
2024-04-26 14:00:10 -0500

    Remove k3sup after installation

--- kubernetes/k3s-deploy/k3s.sh
+++ kubernetes/k3s-deploy/k3s.sh
@@ -73,6 +73,7 @@ then
   echo -e " \033[31;5mk3sup not found, installing\033[0m"
   curl -sLS https://get.k3sup.dev | sh
   sudo install k3sup /usr/local/bin/
+  rm k3sup
 else
   echo -e " \033[32;5mk3sup already installed\033[0m"
 fi
