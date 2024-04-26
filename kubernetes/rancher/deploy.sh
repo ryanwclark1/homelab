aa39b77dee8600154e7056960c79f2163b5acb3f
Ryan Clark <ryanc@accentservices.com>
2024-04-26 17:10:42 -0500

    Add namespace creation for Rancher deployment

--- kubernetes/rancher/deploy.sh
+++ kubernetes/rancher/deploy.sh
@@ -4,6 +4,7 @@
 helm repo add rancher-latest https://releases.rancher.com/server-charts/latest
 helm repo update
 
+kubectl create namespace cattle-system
 helm install rancher rancher-latest/rancher \
   --namespace cattle-system \
   --set hostname=rancher.${DOMAIN} \
