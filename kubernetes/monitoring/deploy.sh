004fe283b5e7a7f182b686a05ca3ec58d19ad934
Ryan Clark <ryanc@accentservices.com>
2024-04-28 12:55:13 -0500

    Remove monitoring deployment script

--- kubernetes/monitoring/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# More information: https://github.com/prometheus-community/helm-charts/blob/main/charts/kube-prometheus-stack/README.md
-helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-helm repo update
-
-kubectl create namespace monitoring
-helm install monitoring prometheus-community/kube-prometheus-stack \
-  --namespace monitoring
