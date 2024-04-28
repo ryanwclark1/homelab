206d04fc91f96ccef5f0a6cbace815e9c60db2a7
Ryan Clark <ryanc@accentservices.com>
2024-04-28 12:21:56 -0500

    Add namespace creation for monitoring

--- kubernetes/monitoring/deploy.sh
+++ kubernetes/monitoring/deploy.sh
@@ -4,5 +4,6 @@
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
 
+kubectl create namespace monitoring
 helm install monitoring prometheus-community/kube-prometheus-stack \
   --namespace monitoring
