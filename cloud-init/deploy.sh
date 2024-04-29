40333f61ec84d5df6bf8d751aef9834fa7156a51
Ryan Clark <ryanc@accentservices.com>
2024-04-29 15:16:45 -0500

    Update path to inventory.json in deploy.sh

--- cloud-init/deploy.sh
+++ cloud-init/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define the path to your inventory JSON file
-inventory='../../inventory.json'
+inventory='../inventory.json'
 
 if [ ! -f "$inventory" ]; then
     echo "Inventory file not found at $inventory"
