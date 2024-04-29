2ed3a6e5ba455908247fa3bbe7ea37c94d54e5ce
Ryan Clark <ryanc@accentservices.com>
2024-04-29 11:10:55 -0500

    Update loki deployment script and fix ingress file path

--- kubernetes/loki/deploy.sh
+++ kubernetes/loki/deploy.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+WORKING_DIR=$(dirname "$BASH_SOURCE")
+WORKING_DIR=$(cd "$WORKING_DIR"; pwd)
+
 helm repo add grafana https://grafana.github.io/helm-charts
 helm repo update
 
 helm upgrade --install loki grafana/loki-stack \
   --set grafana.enabled=true,prometheus.enabled=true,prometheus.alertmanager.persistentVolume.enabled=false,prometheus.server.persistentVolume.enabled=false,loki.persistence.enabled=true,loki.persistence.storageClassName=nfs-client,loki.persistence.size=5Gi
 
-kubectl apply -f ingress.yml
+kubectl apply -f -f $WORKING_DIR/helm/ingress.yml
