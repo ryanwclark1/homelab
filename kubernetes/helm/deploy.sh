44f0fd8c4a8e9dce983945cf2eb07b391bad049e
Ryan Clark <ryanc@accentservices.com>
2024-04-29 09:35:38 -0500

    Add helm installation script

---
+++ kubernetes/helm/deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Install helm
+if ! command -v helm version &> /dev/null
+then
+  echo -e " \033[31;5mHelm not found, installing\033[0m"
+  curl -fsSL https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+else
+  echo -e " \033[32;5mHelm already installed\033[0m"
+fi
