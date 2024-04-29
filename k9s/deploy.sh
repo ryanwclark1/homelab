64c581685071c51d27957ba50742fb1c73e8331a
Ryan Clark <ryanc@accentservices.com>
2024-04-28 19:45:00 -0500

    Update k9s deploy script to install build essentials and Go

--- k9s/deploy.sh
+++ k9s/deploy.sh
@@ -1,14 +1,35 @@
 #!/bin/bash
 
-sudo apt install -y build-essentials
+# Stop execution on any error
+set -e
+
+# Update package list and install build essentials
+sudo apt-get update
+sudo apt-get install -y build-essential
+
+# Go to the user's home directory
 cd ~
+
+# Download Go binary
 curl -OL https://go.dev/dl/go1.22.2.linux-amd64.tar.gz
-rm -rf /usr/local/go && tar -C /usr/local -xzf go1.22.2.linux-amd64.tar.gz
-export PATH=$PATH:/usr/local/go/bin
+
+# Remove any existing Go installation and extract the new version
+sudo rm -rf /usr/local/go
+sudo tar -C /usr/local -xzf go1.22.2.linux-amd64.tar.gz
+
+# Add Go to PATH for all future terminal sessions
 echo "export PATH=\$PATH:/usr/local/go/bin" >> ~/.bashrc
+
+# Load the new PATH into the current shell session
 source ~/.bashrc
+
+# Verify Go installation
 go version
-cd ~
+
+# Clone the k9s repository and build it
 git clone https://github.com/derailed/k9s.git
 cd k9s
-make build && ./execs/k9s
+
+# Build k9s and run it
+make build
+./execs/k9s
