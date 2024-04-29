6c4b6913a47261e2b8ad42c5c544c8f6d04ecee4
Ryan Clark <ryanc@accentservices.com>
2024-04-29 11:07:22 -0500

    Update function to handle edge case

---
+++ kubernetes/metallb/deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+inventory='../../inventory.json'
+lbrange=$(jq -r '.lbrange' "$inventory")
+
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.12.1/manifests/namespace.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.14.5/config/manifests/metallb-native.yaml
+# Download ipAddressPool and configure using lbrange above
+curl -sO https://raw.githubusercontent.com/ryanwclark1/homelab/main/kubernetes/k3s-deploy/ipAddressPool
+cat ipAddressPool | sed 's/$lbrange/'$lbrange'/g' > $HOME/ipAddressPool.yaml
+kubectl apply -f $HOME/ipAddressPool.yaml
