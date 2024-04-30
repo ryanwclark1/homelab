762b13858da10f68abacb92568ba9da8bf4be538
Ryan Clark <ryanc@accentservices.com>
2024-04-30 11:41:47 -0500

    Fix placeholder in deploy.sh script

--- kubernetes/kube-prometheus-stack/deploy.sh
+++ kubernetes/kube-prometheus-stack/deploy.sh
@@ -14,8 +14,7 @@ REPO_NAME="kube-prometheus-stack"
 temp_ips='temp_ips.txt'
 yaml_file=$WORKING_DIR/helm/values.yaml
 backup_file="$yaml_file.backup"
-placeholder="$endpoints"
-echo $placeholder
+placeholder='\$endpoints'
 
 
 # URL of the RSS feed
@@ -24,46 +23,59 @@ API_URL="https://artifacthub.io/api/v1/packages/helm/$REPO_OWNER/$REPO_NAME/feed
 # Fetch the latest version number using curl and parse it using xmllint
 response=$(curl -s "$API_URL")
 
-# Check if the curl command succeeded
-if [ $? -eq 0 ]; then
-  echo "Response received, checking content..."
-  # Check if the response contains <title> tags
-  if echo "$response" | grep -q '<title>'; then
-    echo "Valid response, processing..."
-    latest_version=$(echo "$response" | xmllint --xpath 'string(//rss/channel/item[1]/title)' -)
-    if [ -n "$latest_version" ]; then
-      echo "Latest release version of $REPO_NAME: $latest_version"
+get_artifacthub(){
+  # Check if the curl command succeeded
+  if [ $? -eq 0 ]; then
+    echo "Response received, checking content..."
+    # Check if the response contains <title> tags
+    if echo "$response" | grep -q '<title>'; then
+      echo "Valid response, processing..."
+      latest_version=$(echo "$response" | xmllint --xpath 'string(//rss/channel/item[1]/title)' -)
+      if [ -n "$latest_version" ]; then
+        echo "Latest release version of $REPO_NAME: $latest_version"
+      else
+        echo "Failed to extract version, please check the XML structure and XPath."
+      fi
     else
-      echo "Failed to extract version, please check the XML structure and XPath."
+      echo "Invalid response, no <title> tags found."
+      echo "$response"
     fi
   else
-    echo "Invalid response, no <title> tags found."
+    echo "Failed to fetch the RSS feed."
     echo "$response"
   fi
-else
-  echo "Failed to fetch the RSS feed."
-  echo "$response"
-fi
+}
 
-# Create secret using .env file
-if [ -f "$WORKING_DIR/.env" ]; then
-  kubectl create secret generic grafana-admin-credentials \
-  --from-env-file="$WORKING_DIR/.env" \
-  --namespace $NAME_SPACE
-  echo "Grafana admin credentials created successfully."
-else
-  echo "No .env file found at $WORKING_DIR"
-  exit 1
-fi
+create_secret(){
+  if [ -f "$WORKING_DIR/.env" ]; then
+    kubectl create secret generic grafana-admin-credentials \
+    --from-env-file="$WORKING_DIR/.env" \
+    --namespace $NAME_SPACE
+    echo "Grafana admin credentials created successfully."
+  else
+    echo "No .env file found at $WORKING_DIR"
+    exit 1
+  fi
+}
 
-# Backup the original YAML file
-if [ -f "$yaml_file" ]; then
+backup_file(){
+  if [ -f "$yaml_file" ]; then
     cp "$yaml_file" "$backup_file"
     echo "Backup of values.yaml created."
-else
+  else
     echo "values.yaml does not exist. Exiting."
     exit 1
-fi
+  fi
+}
+
+get_artifacthub
+
+# Create secret using .env file
+create_secret
+
+# Backup the original YAML file
+backup_file
+
 
 # Extract IP addresses and format them
 mapfile -t all < <(jq -r '.nodes[].vms[].ip' "$inventory")
@@ -83,8 +95,6 @@ fi
 sed -i "/$placeholder/r $temp_ips" "$yaml_file"
 sed -i "/$placeholder/d" "$yaml_file"
 
-echo "Temp IPs:"
-cat $temp_ips
 
 echo -e " \033[32;5mGrafana admin credentials\033[0m"
 kubectl describe secret -n $NAME_SPACE grafana-admin-credentials
