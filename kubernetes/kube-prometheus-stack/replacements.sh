80e5176065fb369c4800a8c71de115b7f829cbac
Ryan Clark <ryanc@accentservices.com>
2024-04-29 19:43:44 -0500

    Update kube-prometheus-stack values.yaml with dynamic endpoints

---
+++ kubernetes/kube-prometheus-stack/replacements.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+WORKING_DIR=$(dirname "$BASH_SOURCE")
+WORKING_DIR=$(cd "$WORKING_DIR"; pwd)
+
+# Path to the inventory JSON file
+inventory='../../inventory.json'
+
+# Temporary file to hold IPs
+temp_ips='temp_ips.txt'
+
+# YAML file to update
+yaml_file='$WORKING_DIR/helm/values.yaml'
+
+# Placeholder in the YAML file to replace
+placeholder='$endpoints'
+
+# Extract IP addresses and format them
+mapfile -t all < <(jq -r '.nodes[].vms[].ip' "$inventory")
+for ip in "${all[@]}"; do
+    echo "- $ip"
+done > "$temp_ips"
+
+# Replace placeholder in the YAML file with the list of IPs
+sed -i "/$placeholder/r $temp_ips" "$yaml_file"
+sed -i "/$placeholder/d" "$yaml_file"
+
+# Remove the temporary file
+rm "$temp_ips"
+
+echo "YAML file updated successfully."
