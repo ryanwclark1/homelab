9204ec9348178350e1bcaa0918a24cf5525ebed6
Ryan Clark <ryanc@accentservices.com>
2024-05-01 12:41:06 -0500

    Refactor deploy script and update Kubernetes configuration

--- kubernetes/kube-prometheus-stack/deploy.sh
+++ kubernetes/kube-prometheus-stack/deploy.sh
@@ -16,59 +16,69 @@ API_URL="https://artifacthub.io/api/v1/packages/helm/$REPO_OWNER/$REPO_NAME/feed
 
 # Check for required files
 if [ ! -f "$inventory" ]; then
-    echo "Inventory file not found at $inventory"
-    exit 1
+  echo "Inventory file not found at $inventory"
+  exit 1
 fi
 
 # Create a Kubernetes secret
 create_secret() {
-    if [ -f "$WORKING_DIR/.env" ]; then
-        kubectl create secret generic grafana-admin-credentials \
-        --from-env-file="$WORKING_DIR/.env" \
-        --namespace $NAME_SPACE
-        echo "Grafana admin credentials created successfully."
-    else
-        echo "No .env file found at $WORKING_DIR."
-        exit 1
-    fi
+  if [ -f "$WORKING_DIR/.env" ]; then
+    kubectl create secret generic grafana-admin-credentials \
+    --from-env-file="$WORKING_DIR/.env" \
+    --namespace $NAME_SPACE
+    echo "Grafana admin credentials created successfully."
+  else
+    echo "No .env file found at $WORKING_DIR."
+    exit 1
+  fi
 }
 
 # Backup YAML file
 backup_file() {
-    if [ -f "$yaml_file" ]; then
-        cp "$yaml_file" "$backup_file"
-        echo "Backup of values.yaml created."
-    else
-        echo "values.yaml does not exist. Exiting."
-        exit 1
-    fi
+  if [ -f "$yaml_file" ]; then
+    cp "$yaml_file" "$backup_file"
+    echo "Backup of values.yaml created."
+  else
+    echo "values.yaml does not exist. Exiting."
+    exit 1
+  fi
+}
+
+
+replace_placeholder() {
+  kubectl get nodes -o jsonpath="{.items[*].metadata.annotations.k3s\.io/internal-ip}"
+  temp_ips='temp_ips.txt'
+  all_ips=$(kubectl get nodes -o jsonpath="{.items[*].metadata.annotations.k3s\.io/internal-ip}")
+  > "$temp_ips"
+  for ip in $all_ips; do
+  echo "    - $ip" >> "$temp_ips"
+  done
+
+  # Update YAML file with IPs
+  if grep -q "$placeholder" "$yaml_file"; then
+    sed -i "/$placeholder/r $temp_ips" "$yaml_file"
+    sed -i "/$placeholder/d" "$yaml_file"
+  else
+    echo "Placeholder '$placeholder' not found in values.yaml. Exiting."
+    exit 1
+  fi
+}
+
+
+
+create_namespace() {
+  if kubectl get ns "$NAME_SPACE" > /dev/null 2>&1; then
+    echo -e "Namespace '$NAME_SPACE' namespace exists, checking installation status..."
+  else
+    echo "Namespace '$NAME_SPACE' does not exist, creating it..."
+    kubectl create namespace "$NAME_SPACE"
+  fi
 }
 
 # Main operations
+create_namespace
 create_secret
-# backup_file
-
-# Extract and format IP addresses
-# mapfile -t all_ips < <(jq -r '.nodes[].vms[].ip' "$inventory")
-# printf "    - %s\n" "${all_ips[@]}" > "$temp_ips"
-
-# # Update YAML file with IPs
-# if grep -q "$placeholder" "$yaml_file"; then
-#     sed -i "/$placeholder/r $temp_ips" "$yaml_file"
-#     sed -i "/$placeholder/d" "$yaml_file"
-# else
-#     echo "Placeholder '$placeholder' not found in values.yaml. Exiting."
-#     exit 1
-# fi
-
-# Ensure the namespace exists before proceeding
-if kubectl get ns "$NAME_SPACE" > /dev/null 2>&1; then
-  echo -e "Namespace '$NAME_SPACE' namespace exists, checking installation status..."
-else
-  echo "Namespace '$NAME_SPACE' does not exist, creating it..."
-  kubectl create namespace "$NAME_SPACE"
-fi
-
+backup_file
 
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
@@ -76,19 +86,19 @@ helm repo update
 # Helm operations
 release_exists=$(helm list -n "$NAME_SPACE" | grep -q 'kube-prometheus-stack')
 if [ $? -ne 0 ]; then
-    echo "No active release found. Installing..."
-    helm install kube-prometheus-stack prometheus-community/kube-prometheus-stack \
-    --namespace $NAME_SPACE \
-    -f "$WORKING_DIR/helm/values.yaml"
-    # --version "${latest_version}"
+  echo "No active release found. Installing..."
+  helm install kube-prometheus-stack prometheus-community/kube-prometheus-stack \
+  --namespace $NAME_SPACE \
+  -f "$WORKING_DIR/helm/values.yaml"
+  # --version "${latest_version}"
 else
-    echo "Release found, upgrading..."
-    helm upgrade --install kube-prometheus-stack prometheus-community/kube-prometheus-stack \
-    --namespace $NAME_SPACE \
-    -f "$WORKING_DIR/helm/values.yaml"
-    # --version "${latest_version}"
+  echo "Release found, upgrading..."
+  helm upgrade --install kube-prometheus-stack prometheus-community/kube-prometheus-stack \
+  --namespace $NAME_SPACE \
+  -f "$WORKING_DIR/helm/values.yaml"
+  # --version "${latest_version}"
 fi
 
 # Restore original YAML file and apply ingress
-# mv "$backup_file" "$yaml_file"
+mv "$backup_file" "$yaml_file"
 kubectl apply -f "$WORKING_DIR/helm/ingress.yaml" -n $NAME_SPACE
