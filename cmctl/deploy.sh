49890bad1b7c4436d31e6a9dd3f193ad9abb2595
Ryan Clark <ryanc@accentservices.com>
2024-05-01 09:44:29 -0500

    Add go and cmctl version checks to deploy script

--- cmctl/deploy.sh
+++ cmctl/deploy.sh
@@ -18,8 +18,11 @@ if ! command -v go version &> /dev/null; then
   if ! grep -q '/usr/local/go/bin' ~/.bashrc; then
       echo "export PATH=\$PATH:/usr/local/go/bin" >> ~/.bashrc
   fi
+  go version
+  source ~/.bashrc
   echo -e " \033[32;5mGo installation complete\033[0m"
 else
+  go version
   echo -e " \033[32;5mGo already installed\033[0m"
 fi
 
@@ -28,7 +31,9 @@ if ! command -v cmctl version &> /dev/null; then
   OS=$(go env GOOS); ARCH=$(go env GOARCH); curl -fsSL -o cmctl https://github.com/cert-manager/cmctl/releases/latest/download/cmctl_${OS}_${ARCH}
   chmod +x cmctl
   sudo mv cmctl /usr/local/bin
+  cmctl version
   echo -e " \033[32;5mcmctl installation complete\033[0m"
 else
+  cmctl version
   echo -e " \033[32;5mcmctl already installed\033[0m"
 fi
