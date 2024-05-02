7da5ba8bb9cdad07f1beb515acffcccbb174e4b6
Ryan Clark <ryanc@accentservices.com>
2024-05-01 23:53:35 -0500

    Update cert-manager deploy script

--- kubernetes/cert-manager/deploy.sh
+++ kubernetes/cert-manager/deploy.sh
@@ -4,19 +4,15 @@ WORKING_DIR=$(dirname "$BASH_SOURCE")
 WORKING_DIR=$(cd "$WORKING_DIR"; pwd)
 
 NAME_SPACE="cert-manager"
-CERT_NAME_SPACE="traefik"
+CERT_NAME_SPACE="default"
 
 REPO_OWNER="cert-manager"
 REPO_NAME="cert-manager"
 
 # GitHub API URL for the latest release
 API_URL="https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/releases/latest"
-
-# Use curl to fetch the latest release data
-# Note: GitHub recommends setting a User-Agent
 response=$(curl -s -H "User-Agent: MyClient/1.0.0" "$API_URL")
 
-# Check if the request was successful
 if echo "$response" | grep -q '"tag_name":'; then
   # Extract the tag name which typically is the version
   latest_version=$(echo "$response" | jq -r '.tag_name')
@@ -35,17 +31,17 @@ fi
 #   kubectl create namespace "$NAME_SPACE"
 # fi
 
-kubectl apply -f "https://github.com/$REPO_OWNER/$REPO_NAME/releases/download/${latest_version}/cert-manager.crds.yaml"
+echo "https://github.com/$REPO_OWNER/$REPO_NAME/releases/download/${latest_version}/cert-manager.crds.yaml"
 
+# kubectl apply -f "https://github.com/$REPO_OWNER/$REPO_NAME/releases/download/${latest_version}/cert-manager.crds.yaml"
 
 export $(cat "$WORKING_DIR/.env" | xargs)
 envsubst < $WORKING_DIR/manifests/secrets/cloudflare-token-secret.yaml | kubectl apply -f -
 envsubst < $WORKING_DIR/manifests/issuers/clusterissuer-staging.yaml | kubectl apply -f -
 envsubst < $WORKING_DIR/manifests/certificates/techcasa-io-staging.yaml | kubectl apply -f -
 
-
 kubectl get svc -n "$NAME_SPACE"
 kubectl get pods -n "$NAME_SPACE"
 kubectl get clusterissuer letsencrypt-staging -o yaml
-kubectl get challenges -n "$CERT_NAME_SPACE"
-kubectl describe order $(kubectl get challenges -n "$CERT_NAME_SPACE" -o jsonpath="{.items[0].metadata.ownerReferences[0].name}") -n "$CERT_NAME_SPACE"
+# kubectl get challenges -n "$CERT_NAME_SPACE"
+# kubectl describe order $(kubectl get challenges -n "$CERT_NAME_SPACE" -o jsonpath="{.items[0].metadata.ownerReferences[0].name}") -n "$CERT_NAME_SPACE"
