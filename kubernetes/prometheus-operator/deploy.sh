0b3983ed33acc345b665871d26eff52030d81fc6
Ryan Clark <ryanc@accentservices.com>
2024-05-02 12:52:15 -0500

    Update Prometheus Operator deployment and namespace***

--- kubernetes/prometheus-operator/deploy.sh
+++ kubernetes/prometheus-operator/deploy.sh
@@ -3,16 +3,16 @@
 WORKING_DIR=$(dirname "$BASH_SOURCE")
 WORKING_DIR=$(cd "$WORKING_DIR"; pwd)
 
-LATEST=$(curl -s https://api.github.com/repos/prometheus-operator/prometheus-operator/releases/latest | jq -cr .tag_name)
-curl -sL https://github.com/prometheus-operator/prometheus-operator/releases/download/${LATEST}/bundle.yaml | kubectl create -f -
+kubectl apply -f "$WORKING_DIR/namespace.yaml"
 
-kubectl wait --for=condition=Ready pods -l  app.kubernetes.io/name=prometheus-operator -n default
-
-kubectl apply -f "$WORKING_DIR/crds"
+# Can use create or apply --server-side
+kubectl create -f "$WORKING_DIR/crds"
 kubectl get crds
 
 kubectl apply -f "$WORKING_DIR/rbac"
 
 kubectl apply -f "$WORKING_DIR/deployment"
+
+kubectl wait --for=condition=Ready pods -l  app.kubernetes.io/name=prometheus-operator -n monintoring
 
 kubectl get pods -n monitoring
