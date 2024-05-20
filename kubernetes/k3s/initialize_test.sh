031e5c5de9fc6d52355ea94b25ec469b446a62d5
Ryan Clark <ryanc@accentservices.com>
2024-05-20 15:51:32 -0500

    Refactor storage disk initialization in initialize_test.sh and deploy.sh

--- kubernetes/k3s/initialize_test.sh
+++ kubernetes/k3s/initialize_test.sh
@@ -37,39 +37,52 @@ storage_disk_size=200G
 #############################################
 #            DO NOT EDIT BELOW              #
 #############################################
+for node in "${all[@]}"; do
+  # Check if the current node is in the storage array
+  if [[ " ${storage[*]} " == *" $node "* ]]; then
+    # unset storage_disk_size
+    # storage_disk_size=$(jq -r --arg ip "$node" '.nodes[].vms[] | select(.ip == $ip) | .storage_disk_size' "$inventory")
+    # echo "Storage disk size: $storage_disk_size"
 
-ssh "$host_user@$node" -i ~/.ssh/$cert_name <<'EOF'
-  MOUNT_POINT=/var/lib/longhorn
-  if grep -q "$MOUNT_POINT" /etc/fstab; then
-    echo "Mount point $MOUNT_POINT already exists in /etc/fstab"
-    exit 0
-  fi
-
-  BLK_ID=$(lsblk --json | jq -r '.blockdevices[]? | del(select(has("children"))) | select(.name != null and .type == "disk") | .name')
-  if [ -n "$BLK_ID" ]; then
-    BLK_ID_PATH="/dev/$BLK_ID"
-    echo $BLK_ID_PATH
-    echo 'label: gpt' | sudo sfdisk $BLK_ID_PATH
-    echo ',,L' | sudo sfdisk $BLK_ID_PATH
-    BLK_ID_CHILD=$(lsblk --json | jq -r --arg BLK_ID "$BLK_ID" '.blockdevices[]? | select(.name == $BLK_ID) | .children[0].name')
-    echo $BLK_ID_CHILD
-    BLK_ID_CHILD_PATH="/dev/$BLK_ID_CHILD"
-    sudo mkfs.ext4 -F $BLK_ID_CHILD_PATH
-    PART_UUID=$(lsblk -O --json | jq -r --arg BLK_ID_CHILD_PATH "$BLK_ID_CHILD_PATH" '.blockdevices[]?.children[]? | select(.path == $BLK_ID_CHILD_PATH) | .partuuid')
-    if [ -n "$PART_UUID" ]; then
-      echo $PART_UUID
-      sudo mkdir -p $MOUNT_POINT
-      if ! grep -q "$PART_UUID" /etc/fstab; then
-        echo "PARTUUID=$PART_UUID $MOUNT_POINT ext4 defaults 0 0" | sudo tee -a /etc/fstab
-      else
-        echo 'UUID already in /etc/fstab'
+    ssh $host_user@$node -i ~/.ssh/$cert_name <<EOF
+      MOUNT_POINT=/var/lib/longhorn
+      if grep -q "$MOUNT_POINT" /etc/fstab; then
+        echo "Mount point $MOUNT_POINT already exists in /etc/fstab"
+        exit 0
       fi
-      # sudo systemctl daemon-reload
-      # sudo reboot
-    else
-      echo 'PART_UUID is null, not updating /etc/fstab'
-    fi
-  else
-    echo 'BLK_ID is null, not proceeding with disk setup'
-  fi
+
+      BLK_ID=$(lsblk --json | jq -r '.blockdevices[]? | del(select(has("children"))) | select(.name != null and .type == "disk") | .name')
+      if [ -n "$BLK_ID" ]; then
+        BLK_ID_PATH="/dev/$BLK_ID"
+        echo $BLK_ID_PATH
+        echo 'label: gpt' | sudo sfdisk $BLK_ID_PATH
+        echo ',,L' | sudo sfdisk $BLK_ID_PATH
+        BLK_ID_CHILD=$(lsblk --json | jq -r --arg BLK_ID "$BLK_ID" '.blockdevices[]? | select(.name == $BLK_ID) | .children[0].name')
+        echo $BLK_ID_CHILD
+        BLK_ID_CHILD_PATH="/dev/$BLK_ID_CHILD"
+        sudo mkfs.ext4 -F $BLK_ID_CHILD_PATH
+        PART_UUID=$(lsblk -O --json | jq -r --arg BLK_ID_CHILD_PATH "$BLK_ID_CHILD_PATH" '.blockdevices[]?.children[]? | select(.path == $BLK_ID_CHILD_PATH) | .partuuid')
+        if [ -n "$PART_UUID" ]; then
+          echo $PART_UUID
+          sudo mkdir -p $MOUNT_POINT
+          if ! grep -q "$PART_UUID" /etc/fstab; then
+            echo "PARTUUID=$PART_UUID $MOUNT_POINT ext4 defaults 0 0" | sudo tee -a /etc/fstab
+          else
+            echo 'UUID already in /etc/fstab'
+          fi
+          sudo mount -a
+          sudo systemctl daemon-reload
+          sudo findmnt --verify --verbose
+          # sudo reboot
+        else
+          echo 'PART_UUID is null, not updating /etc/fstab'
+        fi
+      else
+        echo 'BLK_ID is null, not proceeding with disk setup'
+      fi
 EOF
+  fi
+  echo -e " \033[32;5mNode: $node Initialized!\033[0m"
+done
+
+echo -e " \033[32;5mAll nodes initialized!\033[0m"
