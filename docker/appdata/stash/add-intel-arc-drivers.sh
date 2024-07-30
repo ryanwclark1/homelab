93b3318117b9b1b53d96c48eaffb691b79eb6497
Ryan Clark <ryanwclark@yahoo.com>
2024-07-30 17:26:30 -0500

    chore: Update Docker configuration to include Intel ARC drivers and dependencies

--- docker/appdata/stash/add-intel-arc-drivers.sh
+++ docker/appdata/stash/add-intel-arc-drivers.sh
@@ -2,17 +2,22 @@
 
 apk update
 apk add --no-cache \
+  onevpl \
   onevpl-libs \
   onevpl-dev \
-  onevpl \
+  onevpl-doc \
   svt-av1 \
   svt-av1-dev \
+  libva \
+  libva-dev \
   libva-utils \
+  libva-glx \
+  libva-glx-dev \
   intel-media-sdk \
   intel-media-sdk-dev \
   intel-media-driver \
   intel-media-driver-dev \
   intel-gmmlib \
-  libva-intel-driver \
-
-  # libva libdav1d
+  libva-intel-driver
+apk add onevpl-intel-gpu onevpl-intel-gpu-dev  --repository=https://dl-cdn.alpinelinux.org/alpine/edge/testing
+  #  libdav1d
