40d61f27435095a0dc63e9a01de9395c0660dda5
Ryan Clark <ryanwclark@yahoo.com>
2024-07-30 13:33:36 -0500

    Update stash app

--- docker/appdata/stash/add-intel-arc-drivers.sh
+++ docker/appdata/stash/add-intel-arc-drivers.sh
@@ -1,3 +1,5 @@
 #!/command/with-contenv sh
 
-apk add --no-cache intel-media-driver intel-gmmlib libva libva-intel-driver libva-utils onevpl libdav1d
+apk update
+apk add --no-cache onevpl-libs onevpl-dev onevpl svt-av1 svt-av1-dev
+# intel-media-driver intel-gmmlib libva libva-intel-driver libva-utils onevpl libdav1d
