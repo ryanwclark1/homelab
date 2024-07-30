5ae0e49860d9a902b3cf776ba349ef0fa163005e
Ryan Clark <ryanwclark@yahoo.com>
2024-07-30 14:50:41 -0500

    chore: Update Docker configuration for ARR services and Intel ARC driver initialization script in Stash container

--- docker/appdata/stash/add-intel-arc-drivers.sh
+++ docker/appdata/stash/add-intel-arc-drivers.sh
@@ -1,5 +1,5 @@
 #!/command/with-contenv sh
 
 apk update
-apk add --no-cache onevpl-libs onevpl-dev onevpl svt-av1 svt-av1-dev
+apk add --no-cache onevpl-libs onevpl-dev onevpl svt-av1 svt-av1-dev libva-utils
 # intel-media-driver intel-gmmlib libva libva-intel-driver libva-utils onevpl libdav1d
